e587fba84f90c428b6c7e4df2a84a98ecfd244be
peterparker <petea007s@gmail.com>
2022-12-26 17:04:50 -0800

    внес правки с видео

---
+++ js-lesson-one/work-on-lesson/main.js
@@ -0,0 +1,56 @@
+//Переменные
+const person = 'Petya';
+
+let name = 15;
+name = 22;
+
+console.log(name);
+
+const colors =['green', 'blue', 'red', 'black'];
+
+user = {
+    id: 1,
+    firstName: 'Alex'
+};
+let firstName = 'Sergey';
+let lastName = 'Demin';
+
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let tr = true;
+let fl = false;
+//Операторы
+
+// IF
+if(10>5) {
+ console.log(8)
+};
+
+//IF ELSE
+if(10<5) {
+    console.log(8)
+} else {
+    console.log(7)
+}
+
+//IF ELSE IF
+if(10<5) {
+    console.log(8)
+} else if(8==8) {
+    console.log(7)
+} else {
+    console.log(7)
+}
+//SWITCH CASE
+let color = 'green';
+switch (color) {
+    case 'green' :
+        console.log('Этот цвет зеленый')
+        break;
+    case 'red' :
+        console.log('Этот цвет красный')
+        break;
+    default :
+        console.log('Error')
+}
