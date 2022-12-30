5eba9bafc86b501a1ba340dc3b0216e59a37a766
peterparker <petea007s@gmail.com>
2022-12-30 15:25:40 -0800

    add src

--- site-sport-shop/one-ex/js/main.js
+++ site-sport-shop/one-ex/js/main.js
@@ -5,15 +5,15 @@ const threeImg = document.getElementById('threeImg');
 
 
 oneImg.addEventListener('click', () => {
-    document.querySelector('.big-img').src="img/bike3.png";
+    document.querySelector('.big-img').src=oneImg.src="img/bike3.png";
 });
 
 twoImg.addEventListener('click', () => {
-    document.querySelector('.big-img').src="img/bike1.png";
+    document.querySelector('.big-img').src=twoImg.src="img/bike1.png";
 });
 
 threeImg.addEventListener('click', () => {
-    document.querySelector('.big-img').src="img/bike2.png";
+    document.querySelector('.big-img').src=threeImg.src="img/bike2.png";
 });
 
 
