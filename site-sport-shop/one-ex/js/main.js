0200b82eb26f6f5e8d3594deafaa06c9ae435356
peterparker <petea007s@gmail.com>
2023-01-03 16:26:14 -0800

    add right part

--- site-sport-shop/one-ex/js/main.js
+++ site-sport-shop/one-ex/js/main.js
@@ -1,19 +1,17 @@
+
 const oneImg = document.getElementById('oneImg');
 const twoImg = document.getElementById('twoImg');
 const threeImg = document.getElementById('threeImg');
+const bigImg = document.getElementById('big-img')
 
+function onImageClick () {
+    bigImg.src = this.src;
+};
 
+function clicker  (firstName)  {
+    firstName.addEventListener('click',onImageClick );
+};
 
-oneImg.addEventListener('click', () => {
-    document.querySelector('.big-img').src=oneImg.src="img/bike3.png";
-});
-
-twoImg.addEventListener('click', () => {
-    document.querySelector('.big-img').src=twoImg.src="img/bike1.png";
-});
-
-threeImg.addEventListener('click', () => {
-    document.querySelector('.big-img').src=threeImg.src="img/bike2.png";
-});
-
-
+clicker(oneImg);
+clicker(twoImg);
+clicker(threeImg);
