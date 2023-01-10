75ae3275f5426431885b35a907b80a117ee8a473
peterparker <petea007s@gmail.com>
2023-01-09 17:18:42 -0800

    add right part

--- site-sport-shop/one-ex/js/main.js
+++ site-sport-shop/one-ex/js/main.js
@@ -15,3 +15,30 @@ function clicker  (firstName)  {
 clicker(oneImg);
 clicker(twoImg);
 clicker(threeImg);
+// ________________________________________________________________ NEW PART
+const pink = document.getElementById('pink')
+const violet = document.getElementById('violet')
+const yellow = document.getElementById('yellow')
+
+pink.addEventListener('click', () => {
+    pink.classList.add('active')
+
+    violet.classList.remove('active')
+    yellow.classList.remove('active')
+})
+
+violet.addEventListener('click', () => {
+    violet.classList.add('active')
+
+    pink.classList.remove('active')
+    yellow.classList.remove('active')
+})
+
+yellow.addEventListener('click', () => {
+    yellow.classList.add('active')
+
+    pink.classList.remove('active')
+    violet.classList.remove('active')
+})
+
+
